3f8ece681a0244938629feeef06d8ec68fdc8e05
Aaditya <aaditya@Aadityas-MacBook-Pro.local>
2025-09-04 21:59:42 +0530

    Test template 2969 (UTILITY, single variable)

--- sms_email.py
+++ sms_email.py
@@ -45,9 +45,9 @@ def send_whatsapp_reminder(mobile, student_name, plan, expiry_date, include_qr=T
     
     params = {
         "authorization": FAST2SMS_API_KEY,
-        "message_id": "3004",
+        "message_id": "2969",
         "numbers": mobile,
-        "variables_values": variables
+        "variables_values": f"{student_name}"
     }
     
     try:
