1ce327f4ef29f0eb2d8e65a2092bfdf8ff8df003
Aaditya <aaditya@Aadityas-MacBook-Pro.local>
2025-09-05 14:58:15 +0530

    Add installment reminder template 5197 with correct 5-variable format

--- sms_email.py
+++ sms_email.py
@@ -13,7 +13,8 @@ def send_whatsapp(template, mobile, variables_list):
     
     template_map = {
         "fee_reminder": "5170",
-        "payment_receipt": "5171"
+        "payment_receipt": "5171",
+        "installment_reminder": "5197"
     }
 
     message_id = template_map.get(template)
@@ -71,6 +72,25 @@ def send_whatsapp_reminder(mobile, student_name, plan, expiry_date, include_qr=F
     variables_list = [student_name, plan, expiry_date_formatted]
     
     return send_whatsapp("fee_reminder", mobile, variables_list)
+
+def send_whatsapp_installment_reminder(mobile, student_name, pending_amount, due_date, plan, package_end_date):
+    """Send installment reminder using template 5197 - 5 variables: Var1|Var2|Var3|Var4|Var5"""
+    
+    # Format dates to dd-mm-yyyy
+    try:
+        due_date_formatted = datetime.strptime(str(due_date), '%Y-%m-%d').strftime('%d-%m-%Y')
+    except:
+        due_date_formatted = str(due_date)
+    
+    try:
+        end_date_formatted = datetime.strptime(str(package_end_date), '%Y-%m-%d').strftime('%d-%m-%Y')
+    except:
+        end_date_formatted = str(package_end_date)
+    
+    # Template 5197: Var1=student_name|Var2=pending_amount|Var3=due_date|Var4=plan|Var5=package_end_date
+    variables_list = [student_name, str(pending_amount), due_date_formatted, plan, end_date_formatted]
+    
+    return send_whatsapp("installment_reminder", mobile, variables_list)
 
 def send_sms_receipt(mobile, student_name, amount, receipt_no, next_due_info):
     """Send SMS receipt using Fast2SMS"""
